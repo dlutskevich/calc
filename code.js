495b6ad4e5e75126236aaa80f82ae3b22f61e57b
rzoro <qsam4you@gmail.com>
2020-05-03 20:47:01 +0300

    renamed sum function

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-function sum(num, num1, oper) {
+function calc(num, num1, oper) {
     switch (oper) {
         case "+": {
             return num + num1;
@@ -23,5 +23,5 @@ const num = parseInt(prompt("Введите первое число"));
 const num1 = parseInt(prompt("Введите второе число"));
 const oper = prompt("Введите оператор +, -, /, *");
 
-let result = sum(num, num1, oper);
+let result = calc(num, num1, oper);
 alert(result);
